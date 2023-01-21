fc18cabbfc05250e4fb8040ba6ec7f4d903560d7
christopher lopez <christopherwlopez89@gmail.com>
2023-01-21 15:25:59 -0800

    got vendor driver and hub working together with socketIO

--- src/hub.js
+++ src/hub.js
@@ -1,5 +1,6 @@
 
 const { Server } = require("socket.io");
+const { EVENT_NAMES } = require("./utils");
 const io = new Server(3333)
 
 // startDriver();
@@ -7,11 +8,25 @@ const io = new Server(3333)
 
 
 function startEventServer(){
-    io.on("connection",()=>{
-        console.log("have new connection");
-    });
+    // socket describes who is attached to it
+    io.on("connection",(socket)=>{
+        console.log("have new connection", socket.id);
+        socket.join("general");
+
     
+
+    // BUSY WORK! whenever the hub gets a pickup or delivery event, send it to everyone
+    socket.on(EVENT_NAMES.delivered, (delivered)=>{
+        console.log("HUB delivered", delivered);
+        io.to("general").emit(EVENT_NAMES.delivered,delivered);
+    });
+
+    socket.on(EVENT_NAMES.pickup, (pickup)=>{
+        console.log("HUB pickup", pickup);
+        io.to("general").emit(EVENT_NAMES.pickup, pickup);
+    });
+
+});
     console.log("everything is started")
 }
-
 startEventServer();
